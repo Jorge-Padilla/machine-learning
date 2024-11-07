a7cba629ac5651916a9b4fd35a5508a2905cae58
Jorge-Padilla <jorgeivei08@hotmail.com>
2024-11-06 18:23:19 -0600

    updates to project

---
+++ notebooks/fix.py
@@ -0,0 +1,5 @@
+#!C:\Users\japadill\AppData\Local\anaconda3\envs\iteso2024\python.exe
+from __future__ import absolute_import
+import sys
+from trashcli.put.main import main as main
+sys.exit(main())
